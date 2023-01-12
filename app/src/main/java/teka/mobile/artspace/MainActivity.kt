e0d402b397dc2a55ff7c5d11ed51ee926812f483
samAricha <samaricha001@gmail.com>
2023-01-12 19:30:52 +0300

    Basic Layout

---
+++ app/src/main/java/teka/mobile/artspace/MainActivity.kt
@@ -0,0 +1,88 @@
+package teka.mobile.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import teka.mobile.artspace.ui.theme.ArtSpaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArtSpaceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ArtSpace()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ArtSpaceTheme {
+        ArtSpace()
+    }
+}
+
+@Composable
+fun ArtSpace(modifier: Modifier = Modifier){
+    var counterNum by remember {
+        mutableStateOf(1)
+    }
+    val imageResource = when(counterNum){
+        1 -> R.drawable.male1
+        2 -> R.drawable.male2
+        3 -> R.drawable.male3
+        else -> R.drawable.male4
+
+    }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally){
+        Image(painter = painterResource(id = imageResource), contentDescription = "")
+        Spacer(modifier = modifier.height(16.dp))
+        Text(text = counterNum.toString())
+        Spacer(modifier = modifier.width(16.dp))
+        Text(text = "Sarufi 001")
+        Text(text = "2020")
+        modifier.height(16.dp)
+        Row() {
+            Button(onClick = { counterNum = counterRegulator(false, counterNum) }, modifier.width(120.dp)) {
+                Text(stringResource(id = R.string.previous_btn))
+            }
+            Spacer(modifier = modifier.width(16.dp))
+            Button(onClick = { counterNum = counterRegulator(true, counterNum) }, modifier.width(120.dp)) {
+                Text(stringResource(id = R.string.next_btn))
+            }
+        }
+    }
+}
+
+fun counterRegulator(next: Boolean, currentCounter: Int): Int{
+    var returnValue: Int = 0
+    if(next){
+       returnValue++
+    }else{
+        returnValue--
+    }
+    return returnValue
+}
